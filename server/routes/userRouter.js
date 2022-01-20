76d5e6feca78e3a3c6ce86e3dd88c5e7d67d3ee3
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-01-20 11:16:05 +0100

    add AuthMiddleware

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,4 +1,5 @@
 const Router = require('express');
+const authMiddleware = require('../middleware/AuthMiddleware');
 const userController = require('../controllers/userController');
 
 const router = new Router();
@@ -8,6 +9,6 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/profile', userController.profile);
+router.get('/profile', authMiddleware, userController.profile);
 
 module.exports = router;
