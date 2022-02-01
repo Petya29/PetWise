1d8ce89222e0c22e3badf74ea032bb5ef892eb73
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-02-01 22:34:43 +0100

    add preloader on home page

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ProductService from '../services/ProductService';
 import ProductsList from '../components/products/ProductsList';
+import AppLoader from '../components/UI/AppLoader/AppLoader';
 
 export default function Home() {
 
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   let getProducts = useRef(async () => { });
 
   getProducts = async () => {
     try {
+      setLoading(true);
+
       const response = await ProductService.getProducts();
       setProducts(response.data);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
     } catch (e) {
       console.log(e);
+      setLoading(false);
     }
   }
 
@@ -24,7 +33,14 @@ export default function Home() {
 
   return (
     <div className="home-view">
-      <ProductsList products={products} />
+      {loading
+        ?
+        <div className="home-loader-wrap">
+          <AppLoader style={{ width: "50px", height: "50px" }} color="#ffffff" />
+        </div>
+        :
+        <ProductsList products={products} />
+      }
     </div>
   )
 }
